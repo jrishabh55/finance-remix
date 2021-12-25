35d8319c8e44df9c3678941e95b8373fb660a071
Rishabh Jain <rishabh@codeation.io>
2021-12-25 19:57:59 +0530

    feat: taiwind plugins and config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ["./app/**/*.{ts,tsx}"],
+  content: ['./app/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#4ECCA3',
+      secondary: '#EEEEEE',
+      background: '#393E46',
+      black: '#232931'
+    }
   },
-  plugins: [],
-}
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio')
+  ]
+};
