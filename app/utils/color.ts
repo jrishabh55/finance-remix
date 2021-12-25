080aecf060a6e18d54bf8d5a0329f3de6771d723
Rishabh Jain <rishabh@codeation.io>
2021-12-26 01:05:35 +0530

    feat: color utilities

---
+++ app/utils/color.ts
@@ -0,0 +1,7 @@
+const colors = { primary: '#4ECCA3', red: '#F44336', orange: '#FF9800', green: '#4CAF50' };
+const borderColors = { primary: '#4ECCA3', red: '#F44336', orange: '#FF9800', green: '#4CAF50' };
+
+export const chartColors = Object.values(colors);
+export const chartBorderColors = Object.values(borderColors);
+
+export default colors;
