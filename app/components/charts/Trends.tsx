35441bc1683be1bb99b145c01dbb3dbeace48841
Rishabh Jain <rishabh@codeation.io>
2021-12-26 02:00:37 +0530

    feart: added trends chart

--- app/components/charts/Trends.tsx
+++ app/components/charts/Trends.tsx
@@ -1,100 +1,29 @@
 import numeral from 'numeral';
-import { PureComponent } from 'react';
+import { FC } from 'react';
 import Chart from '~/components/Charts';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-];
+export type TrendsData = { deposit: number; withdrawal: number; name: string }[];
 
-export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
+const Trends: FC<{ data?: TrendsData; title?: string }> = ({ title, data = [] }) => {
+  return (
+    <Chart
+      id="bar-chart"
+      type="bar"
+      title={title}
+      axis={[
+        { aType: 'x', dataKey: 'name' },
+        {
+          aType: 'y',
+          tickFormatter: (value: number) => numeral(value).format('0.0a')
+        }
+      ]}
+      series={[
+        { type: 'bar', dataKey: 'deposit' },
+        { type: 'bar', dataKey: 'withdrawal' }
+      ]}
+      data={data}
+    />
+  );
+};
 
-  render() {
-    return (
-      // <ResponsiveContainer width="100%" height="100%">
-      // <LineChart
-      //   width={500}
-      //   height={300}
-      //   data={data}
-      //   margin={{
-      //     top: 5,
-      //     right: 30,
-      //     left: 20,
-      //     bottom: 5
-      //   }}>
-      //   <CartesianGrid strokeDasharray="3 3" />
-      //   <XAxis dataKey="name" />
-      //   <YAxis />
-      //   <Tooltip />
-      //   <Legend />
-      //   <Line
-      //     isAnimationActive={false}
-      //     type="monotone"
-      //     dataKey="pv"
-      //     stroke="#8884d8"
-      //     activeDot={{ r: 8 }}
-      //   />
-      //   <Line isAnimationActive={false} type="monotone" dataKey="uv" stroke="#82ca9d" />
-      // </LineChart>
-      // </ResponsiveContainer>
-      <Chart
-        id="bar-chart"
-        type="bar"
-        axis={[
-          { aType: 'x', dataKey: 'name' },
-          {
-            aType: 'y',
-            dataKey: 'pv',
-            tickFormatter: (value: number) => numeral(value).format('0.0a')
-          }
-        ]}
-        series={[
-          { type: 'bar', dataKey: 'uv' },
-          { type: 'bar', dataKey: 'pv' }
-        ]}
-        data={data}
-      />
-    );
-  }
-}
+export default Trends;
