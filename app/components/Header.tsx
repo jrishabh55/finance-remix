6c0a2f747c7d2055b0e5b00dd6825f8026fba3d1
Rishabh Jain <rishabh@codeation.io>
2021-12-26 00:10:26 +0530

    header and sidebar

---
+++ app/components/Header.tsx
@@ -0,0 +1,55 @@
+import { FC } from 'react';
+import { Link } from 'remix';
+
+const Header: FC<{ onSidebarChange: () => void; sidebar: boolean }> = ({
+  onSidebarChange,
+  sidebar
+}) => {
+  return (
+    <header className="flex flex-col bg-black">
+      <div className={`pt-2`}>
+        <div className="flex flex-col flex-1 max-w-xs w-full pt-5 pb-4 px-6 md:pt-0">
+          <div className="flex items-center space-x-3 px-2">
+            <Link
+              to="/"
+              className="flex items-center no-underline font-bold text-2xl text-gray-800 hover:text-gray-600 focus:outline-none focus:text-gray-600">
+              <img
+                src="https://avatars.githubusercontent.com/u/18290665?v=4"
+                alt="logo"
+                className="h-12 rounded-full"
+              />
+              <span className="ml-2">Finance RJ</span>
+            </Link>
+            <button
+              className="flex items-center no-underline text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700"
+              onClick={onSidebarChange}>
+              <svg
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round">
+                {!sidebar ? (
+                  <>
+                    <line x1="4" y1="6" x2="20" y2="6"></line>
+                    <line x1="4" y1="12" x2="20" y2="12"></line>
+                    <line x1="4" y1="18" x2="20" y2="18"></line>
+                  </>
+                ) : (
+                  <>
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                  </>
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { Link } from 'remix';
+
+export const Sidebar = () => {
+  return (
+    <aside className={`w-56 max-w-[14rem] pt-2 bg-black flex-1 px-2 nav-transition`}>
+      <ul className="space-y-4">
+        <NavItem to="/">Home</NavItem>
+        <NavItem to="/finance">Finance</NavItem>
+      </ul>
+    </aside>
+  );
+};
+
+export const NavItem: FC<{ to: string }> = ({ children, to }) => {
+  return (
+    <li className="text-center py-4 my-2 md:my-0 dark:shadow-lg shadow-background border-b border-background">
+      <Link
+        to={to}
+        className="block mt-1 text-gray-800 hover:text-gray-600 no-underline hover:no-underline">
+        {children}
+      </Link>
+    </li>
+  );
+};
