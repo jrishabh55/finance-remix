78862a2b134074d14ba9e19e5de645a25f54f2b4
Rishabh Jain <rishabh@codeation.io>
2022-01-11 16:49:56 +0530

    chore: sidebar user layout update

--- app/containers/UserLayout.tsx
+++ app/containers/UserLayout.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Footer from '~/components/Footer';
 import Header from '~/components/Header';
 import { Sidebar } from '~/components/Sidebar';
@@ -6,11 +6,20 @@ import { Sidebar } from '~/components/Sidebar';
 const UserLayout: FC = ({ children }) => {
   const [sidebar, setSidebar] = useState(false);
 
+  useEffect(() => {
+    sessionStorage.getItem('sidebar') === 'true' && setSidebar(true);
+  }, []);
+
+  const handleSidebarToggle = () => {
+    sessionStorage.setItem('sidebar', sidebar ? 'false' : 'true');
+    setSidebar(!sidebar);
+  };
+
   return (
     <main className="flex flex-col min-h-screen w-screen max-w-full">
-      <Header onSidebarChange={() => setSidebar(!sidebar)} sidebar={sidebar} />
+      <Header onSidebarChange={handleSidebarToggle} sidebar={sidebar} />
       <main className="flex flex-row flex-grow">
-        {sidebar && <Sidebar />}
+        {<Sidebar show={sidebar} />}
         <section className="flex-grow nav-transition p-2 pb-10">{children}</section>
       </main>
       <Footer />
