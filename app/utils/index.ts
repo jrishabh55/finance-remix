69a0afd8ee17d4f26687d780966f3ea9d1a3def6
Rishabh Jain <rishabh@codeation.io>
2022-01-16 17:14:55 +0530

    feat: excerpt utill

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -16,3 +16,10 @@ export const months = [
 export const randomNumber = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export const excerpt = (text: string, length: number = text.length) => {
+  if (text.length > length) {
+    return text.substring(0, length) + '...';
+  }
+  return text;
+};
