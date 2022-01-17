a3e78ffa2cdd1fcb0e55dfedab519f5f2ad283eb
Rishabh Jain <rishabh@codeation.io>
2022-01-17 17:00:00 +0530

    fix: modal z-ratios

--- app/lib/Modal.tsx
+++ app/lib/Modal.tsx
@@ -29,21 +29,24 @@ const Modal: FC<ModalProps> = ({ children, title, onClose, onOpen }) => {
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog
           as="div"
-          className="fixed inset-0 z-10 bg-background/90 overflow-y-auto"
+          className="fixed inset-0 bg-background/90 overflow-y-auto"
           onClose={closeModal}>
           <div className="px-4">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0">
-              <Dialog.Overlay className="fixed inset-0" />
-            </Transition.Child>
-
-            <div className="h-screen z-20 flex items-center justify-center" aria-hidden="true">
+            <div className="h-screen flex items-center justify-center" aria-hidden="true">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0">
+                <Dialog.Overlay className="fixed inset-0 z-50" />
+              </Transition.Child>
+              {/* This element is to trick the browser into centering the modal contents. */}
+              <span className="inline-block h-screen align-middle" aria-hidden="true">
+                &#8203;
+              </span>
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -52,7 +55,9 @@ const Modal: FC<ModalProps> = ({ children, title, onClose, onOpen }) => {
                 leave="ease-in duration-200"
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95">
-                <div className="flex rounded-lg border-primary border-solid -mt-72">{children}</div>
+                <div className="flex rounded-lg border-primary border-solid z-[100]">
+                  {children}
+                </div>
               </Transition.Child>
             </div>
           </div>
