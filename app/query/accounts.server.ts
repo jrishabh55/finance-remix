25cc0cb7030fa34e2c2e1f5b283e36732c8ec616
Rishabh Jain <rishabh@codeation.io>
2022-02-10 22:16:59 +0530

    feat: added dashboard accounts sum component

--- app/query/accounts.server.ts
+++ app/query/accounts.server.ts
@@ -9,3 +9,42 @@ export const getAccounts: GetAccounts = async (userId) => {
 
   return accounts || [];
 };
+
+export const getAccountsWithSum = async (userId: string) => {
+  const transactions = await db.transaction.groupBy({
+    by: ['accountId', 'type'],
+    where: { userId },
+    _sum: {
+      amount: true
+    }
+  });
+
+  const accounts = await db.account.findMany({
+    where: { userId },
+    orderBy: {
+      type: 'asc'
+    },
+    select: {
+      id: true,
+      name: true,
+      type: true
+    }
+  });
+
+  const accountsWithSum = accounts.map((account) => {
+    const filteredTransaction = transactions.filter(
+      (transaction) => transaction.accountId === account.id
+    );
+    return {
+      ...account,
+      sum: {
+        deposit: filteredTransaction.find((t) => t.type === 'DEPOSIT')?._sum.amount || 0,
+        expense: filteredTransaction.find((t) => t.type === 'WITHDRAWAL')?._sum.amount || 0
+      }
+    };
+  });
+
+  return accountsWithSum;
+};
+
+export type GetAccountsWithSum = typeof getAccountsWithSum;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,40 @@
 import { LoaderFunction, useLoaderData } from 'remix';
+import AccountList from '~/components/accounts/List';
 import Cashflow, { CashflowData } from '~/components/charts/Cashflow';
 import MonthlyTrends from '~/components/charts/MonthlyTrends';
-import Trends, { TrendsData } from '~/components/charts/Trends';
-import { GridHeader } from '~/components/GridHeader';
-import { Title } from '~/lib/Typography';
-import { monthlyTrends, yearlyTrends } from '~/query/charts.server';
+import { TrendsData } from '~/components/charts/Trends';
+import Card from '~/lib/Card';
+import { getAccountsWithSum, GetAccountsWithSum } from '~/query/accounts.server';
+import { monthlyTrends } from '~/query/charts.server';
 import { months } from '~/utils';
 import { requireUserId } from '~/utils/session.server';
 
-type LoaderData = { cashflow: CashflowData; monthlyTrends: TrendsData; yearly: TrendsData };
+type LoaderData = {
+  cashflow: CashflowData;
+  monthlyTrends: TrendsData;
+  yearly?: TrendsData;
+  accountsWithSum: Awaited<ReturnType<GetAccountsWithSum>>;
+};
 
 export const loader: LoaderFunction = async ({ request }): Promise<LoaderData | Response> => {
-  await requireUserId(request);
+  const userId = await requireUserId(request);
 
-  const yearlyTrendsData = await yearlyTrends();
-  const monthlyTrendsData = await monthlyTrends({ year: new Date().getFullYear() });
+  // const yearlyTrendsData = await yearlyTrends();
+  const [monthlyTrendsData, accountsWithSum] = await Promise.all([
+    monthlyTrends({ year: new Date().getFullYear(), userId }),
+    getAccountsWithSum(userId)
+  ]);
 
-  const yearly: TrendsData = yearlyTrendsData.map(({ deposit, withdrawal, name }) => {
-    const average = deposit - withdrawal;
+  // const yearly: TrendsData = yearlyTrendsData.map(({ deposit, withdrawal, name }) => {
+  //   const average = deposit - withdrawal;
 
-    return {
-      name,
-      deposit,
-      withdrawal: withdrawal * -1,
-      average
-    };
-  });
+  //   return {
+  //     name,
+  //     deposit,
+  //     withdrawal: withdrawal * -1,
+  //     average
+  //   };
+  // });
 
   const cashflow: CashflowData = monthlyTrendsData.map(({ deposit, withdrawal, name }) => {
     const average = deposit - withdrawal;
@@ -48,27 +57,35 @@ export const loader: LoaderFunction = async ({ request }): Promise<LoaderData | 
     };
   });
 
-  return { cashflow, monthlyTrends: parsedMonthlyTrendsData, yearly };
+  return { cashflow, monthlyTrends: parsedMonthlyTrendsData, accountsWithSum };
 };
 
 export default function Index() {
-  const { cashflow, monthlyTrends, yearly } = useLoaderData<LoaderData>();
+  const { cashflow, monthlyTrends, accountsWithSum } = useLoaderData<LoaderData>();
 
   return (
-    <section className="grid grid-cols-12 gap-4 md:grid-cols-12">
-      <GridHeader title="Dashboard">
-        <Title type="page-header">mjisaudg</Title>
-      </GridHeader>
-      <article className="col-span-12 md:col-span-2">
-        <Trends title="Yearly Comparison" data={yearly} small legend={false} />
+    <section className="grid gap-4 md:grid-cols-12">
+      <article className="col-span-6">
+        <Card className="flex-grow" collapsible title="Accounts">
+          <AccountList accounts={accountsWithSum} />
+        </Card>
       </article>
-      <article className="col-span-12 md:col-span-6 md:col-start-7">
+      {/* <article className="col-span-6">
+          <Trends title="Yearly Comparison" data={yearly} small legend={false} />
+        </article> */}
+      <article className="col-span-6">
         <MonthlyTrends deposit={false} title="Expenses" data={monthlyTrends} />
       </article>
-      <article className="col-span-12 md:col-span-6">
+      <article className="col-span-6">
+        <Card title="Working">Accounts</Card>
+      </article>
+      <article className="col-span-6">
         <Cashflow title="Cashflow" data={cashflow} />
       </article>
-      <article className="col-span-12 md:col-span-6">
+      <article className="col-span-6">
+        <Card title="Working">Accounts</Card>
+      </article>
+      <article className="col-span-6">
         <MonthlyTrends title="Monthly Trends" data={monthlyTrends} />
       </article>
     </section>
